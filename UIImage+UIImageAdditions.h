a6205fd995ab0b22aa533accd7ac170ca33dcbb3
wwwins <jacky.huang@isobar.com>
2014-07-24 10:33:18 +0800

    Init import.

---
+++ UIImage+UIImageAdditions.h
@@ -0,0 +1,18 @@
+//
+//  UIImage+UIImageAdditions.h
+//  MyLayoutViewDemo
+//
+//  Created by wwwins on 2014/7/24.
+//  Copyright (c) 2014年 isobar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (UIImageAdditions)
+
+- (UIImage *)resizeImage:(UIImage *)image convertToWidth:(float)width;
+- (UIImage *)resizeImage:(UIImage *)image convertToHeight:(float)height;
+- (UIImage *)resizeImage:(UIImage *)image imageSize:(CGSize)size;
+
+
+@end
---
+++ UIImage+UIImageAdditions.m
@@ -0,0 +1,52 @@
+//
+//  UIImage+UIImageAdditions.m
+//  MyLayoutViewDemo
+//
+//  Created by wwwins on 2014/7/24.
+//  Copyright (c) 2014年 isobar. All rights reserved.
+//
+
+#import "UIImage+UIImageAdditions.h"
+
+@implementation UIImage (UIImageAdditions)
+
+// fix the aspect ratio of the image
+- (UIImage *)resizeImage:(UIImage *)image convertToWidth:(float)width
+{
+  float ratio = image.size.width / width;
+  float height = image.size.height / ratio;
+  CGSize size = CGSizeMake(width, height);
+  //  UIGraphicsBeginImageContext(size);
+  UIGraphicsBeginImageContextWithOptions(size, NO, [UIScreen mainScreen].scale);
+  [image drawInRect:CGRectMake(0, 0, size.width, size.height)];
+  UIImage * newimage = UIGraphicsGetImageFromCurrentImageContext();
+  UIGraphicsEndImageContext();
+  return newimage;
+}
+
+// fix the aspect ratio of the image
+- (UIImage *)resizeImage:(UIImage *)image convertToHeight:(float)height
+{
+  float ratio = image.size.height / height;
+  float width = image.size.width / ratio;
+  CGSize size = CGSizeMake(width, height);
+  //  UIGraphicsBeginImageContext(size);
+  UIGraphicsBeginImageContextWithOptions(size, NO, [UIScreen mainScreen].scale);
+  [image drawInRect:CGRectMake(0, 0, size.width, size.height)];
+  UIImage * newimage = UIGraphicsGetImageFromCurrentImageContext();
+  UIGraphicsEndImageContext();
+  return newimage;
+  
+}
+
+-(UIImage*)resizeImage:(UIImage *)image imageSize:(CGSize)size
+{
+  //UIGraphicsBeginImageContext(size);
+  UIGraphicsBeginImageContextWithOptions(size, NO, [UIScreen mainScreen].scale);
+  [image drawInRect:CGRectMake(0,0,size.width,size.height)];
+  UIImage* newImage = UIGraphicsGetImageFromCurrentImageContext();
+  UIGraphicsEndImageContext();
+  return newImage;
+}
+
+@end
